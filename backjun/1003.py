905892d444ccedb369257f6fbea0523b49f98770
teteo7 <teteocomputer@gmail.com>
2022-07-11 13:20:50 +0900

    soft commit

---
+++ backjun/1003.py
@@ -0,0 +1,8 @@
+a = input("값입력: ")
+print(a)
+int(a)
+
+if(a>40):
+    print("a가 40")
+else:
+    print("ㄷㄱㄷㄱ")
---
+++ main0.py
@@ -0,0 +1,23 @@
+import pyautogui
+import time
+import datetime
+
+for _ in range(0,4):
+
+    checktime = datetime.datetime.now()
+    print(checktime)
+    #x y
+    pyautogui.moveTo(5, 5)
+    time.sleep(3)
+
+    pyautogui.moveTo(1600,85)
+    pyautogui.click()
+    pyautogui.typewrite('A', interval=0.1)
+    pyautogui.typewrite(['a', 'b'], interval=0.1)
+
+    print("check")
+    # 1당 초
+    time.sleep(240)
+
+# pyautogui.moveTo(5,30)
+# pyautogui.click()
